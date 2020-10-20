5b21dece6403dd609abbb436652866dbd6d4230b
jam <jam@zoyi.co>
2020-10-21 01:30:40 +0900

    Updated version to 8.0.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChannelIO",
-            url: "https://mobile-static.channel.io/ios/8.0.0/spm-xcframework.zip",
-            checksum: "37ca83e448441b7419d333e21ed81c032918040f5c717911d4c65b04365f490e")
+            url: "https://mobile-static.channel.io/ios/8.0.1/spm-xcframework.zip",
+            checksum: "75693c141250b903424df0ae4595bff049b84cc5d35331b6cdd03f0bb646bcbb")
     ]
 )
