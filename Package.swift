7ce8105e5d7d15c96cc3c57f7a06ef07223854e2
jam0128 <52900717+jam0128@users.noreply.github.com>
2021-09-27 17:41:45 +0900

    Fixed bundle last

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChannelIOFront",
-            url: "https://cf.exp.channel.io/file/72/6151785e1071983be8e5/channeliofront-xcframework.zip",
-            checksum: "2ff412bd2e09758e9849eaaba7bb714d774912241348de6ec4f0ff51eaebb412"
+            url: "https://cf.exp.channel.io/file/72/6151836d851c3d02a9f2/channeliofront-xcframework.zip",
+            checksum: "f7d02dd8d05424ebab7268482d10ac5d8746ddff910e25bd380bdb055b328c08"
         )
     ],
     swiftLanguageVersions: [.v5]
