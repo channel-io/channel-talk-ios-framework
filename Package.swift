f40caf2a1edb835bf6fc315b166b95c625eba722
jam0128 <52900717+jam0128@users.noreply.github.com>
2022-04-04 22:51:07 +0900

    Updated version

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChannelIOFront",
-            url: "https://cf.channel.io/file/21788/62435db9cb08cb79de7f/channeliofront-xcframework.zip",
-            checksum: "a61acd0b29c61986fcf33152366f6e4707b06ee8c9e47d0e904572fa59c57f89"
+            url: "https://cf.channel.io/file/67631/624af74e65a508508963/channeliofront-xcframework.zip",
+            checksum: "ccf634b2798f1817e6ea1c97879a6ac0441a110fffcfed0c6bf9ed9ebd6e5bd5"
         )
     ],
     swiftLanguageVersions: [.v5]
