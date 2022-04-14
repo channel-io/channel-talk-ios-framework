c18b8aa9b04d8c72be46cd03d2d6a2d13c2a0092
jam0128 <52900717+jam0128@users.noreply.github.com>
2022-04-15 02:52:44 +0900

    Updated version to 10.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "ChannelIOFront",
             url: "https://mobile-static.channel.io/ios/10.0.0/spm-xcframework.zip",
-            checksum: "a751993669fe3d3111464649017d894994135928a1e258df80d7c9b06aeea088"
+            checksum: "9fa250fd6baf0f335b4fb26865467b253445e3e7307e16b8b48613d35c821922"
         )
     ],
     swiftLanguageVersions: [.v5]
