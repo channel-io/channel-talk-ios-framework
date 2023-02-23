bc70ba022c4ba59234c87ed7b74baa4f491cb515
Bonoogi <bnoo1333@gmail.com>
2023-02-23 23:06:58 +0900

    set binaryTarget to local path

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChannelIOFront",
-            url: "https://mobile-static.channel.io/ios/10.2.0/spm-xcframework.zip",
-            checksum: "6a32fd73d21f36d6c2bb759775df65ca6c0a57d9748e634cc3d567b680e0d52c"
+            path: "spm-xcframework.zip"
         ),
         // targets 안에 binaryTarget 하나만 존재할 경우 SPM Framework 추가 목록에 노출되지 않는 버그가 있어
         // 이를 방지하기 위한 Stub target을 추가합니다 - finn. 2023.02.23
