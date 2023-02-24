9885a26ef6e45b08d905a9b8bc03e9277505a304
jam0128 <52900717+jam0128@users.noreply.github.com>
2023-02-24 23:36:28 +0900

    Updated target name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "ChannelIOSDK",
-            targets: ["ChannelIOFront", "ChannelIOSDKTarget"]),
+            targets: ["ChannelIOFront", "_ChannelIOSDKTarget"]),
     ],
     targets: [
         .binaryTarget(
@@ -19,7 +19,7 @@ let package = Package(
         ),
         // NOTE: targets 안에 binaryTarget 하나만 존재할 경우 SPM Framework 추가 목록에 노출되지 않는 버그가 있어
         // 이를 방지하기 위한 Stub target을 추가합니다 - finn. 2023.02.23
-        .target(name: "ChannelIOSDKTarget")
+        .target(name: "_ChannelIOSDKTarget")
     ],
     swiftLanguageVersions: [.v5]
 )
