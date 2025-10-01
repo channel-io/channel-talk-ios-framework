7565640a2d15ef80486106d23c1fcd65580e2919
jam0128 <52900717+jam0128@users.noreply.github.com>
2025-10-01 11:42:59 +0900

    Updated version to 12.14.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChannelIOFront",
-            url: "https://mobile-static.channel.io/ios/12.14.0/spm-xcframework.zip",
+            url: "https://mobile-static.channel.io/ios/12.14.1/spm-xcframework.zip",
             checksum: "3ea52928f465540ef27fc6a6b5b267ac7d02ccabd90439bff16b46eb4e32e945"
         ),
         // NOTE: targets 안에 binaryTarget 하나만 존재할 경우 SPM Framework 추가 목록에 노출되지 않는 버그가 있어
